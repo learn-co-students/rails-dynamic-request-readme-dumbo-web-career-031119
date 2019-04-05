cdb7df7061aaa23d563ca7ba3a0cafb1b842b026
Danielle Alba <danib.sgh@gmail.com>
2019-04-05 14:23:32 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Post.create(title:"this is it", description: "osdvlhapvihwpVHWpov'lwvn;aksdjbv;aksbjv;ibv;ksbjv;akjnvdiha'soivja'pnvalsndv/akjnvdihakbflkbv
+  w;ekjbf;wJVB;kbjvkwjBV;Kwejbv;kejwbv;kejwbF")
